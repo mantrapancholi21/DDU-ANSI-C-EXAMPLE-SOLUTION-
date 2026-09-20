ea9a5c46284d19d64280dff67611b52da1c554ea
Pal Vikani <palvikani05@gmail.com>
2026-09-20 16:47:51 +0530

    Create EXAMPLE-1.C

---
+++ CHAPTER 3 : CONSTANTS, VARIABLES AND DATA TYPES/EXAMPLE-1.C
@@ -0,0 +1,20 @@
+// chapter : 3
+//example : 3.1
+// this program illustrates the use of integer constants on 16-bit machine.
+
+#include <stdio.h>
+int main()
+{
+    printf("integer values\n\n");
+    printf("%d %d %d\n", 32767, 32767+1, 32767+10);
+    // on 16-bit machine, values above 32767 cause overflow. 
+    
+    printf("\n\n");
+
+    //%ld prints long integer values.
+    printf("Long integer values\n\n");
+    printf("%ld %ld %ld\n", 32767L, 32767+1L, 32767+10L);
+    // 'L' tells the compiler to treat the numbers as long integer.
+
+    return 0;
+}
