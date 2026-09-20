c98a2a1ee28d9ff18224d15684260f90e6c763eb
meet-yadav <yadavmeet529@gmail.com>
2026-09-20 21:16:57 +0530

    Add files via upload

---
+++ CHAPTER 8 : ARRAY/EXAMPLE 2.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#define MAXVAL 50 
+#define COUNTER 11
+int main ()
+{
+  float Value[MAXVAL];
+  int i,low,high;
+  int group[COUNTER]= {0,0,0,0,0,0,0,0,0,0,0};
+  /*. . . . . PEADING AND COUNTING. . . . .*/
+
+  for(i=0;i<MAXVAL;i++)
+  {
+    /*. . . . .RIDING OF VALUE . . . . .*/
+    scanf("%f",&Value[i]) ;
+    /*. . . .COUTING FREQUENCY OF GROUPS. . . . .*/
+      ++ group[ (int) (Value[i]/ 10)];
+  }
+  /*. . . . . PRINTING OF FREQUENCY TABLE. . . . .*/
+  printf("\n");
+  printf(" GROUP RANGE FREQUENCY\n\n");
+  for(i=0;i<COUNTER ; i++)
+  { 
+     low = i * 10;
+     if(i==10)
+
+      high = 100;
+
+    else 
+      high = low + 9;
+    
+      printf("%2d %3d to %3d %3d \n", i+1,low,high, group[i]);
+
+  }  
+}
+
+
+
+
