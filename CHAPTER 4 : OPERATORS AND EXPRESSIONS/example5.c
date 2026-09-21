ec31bd76b055ddde9616cbbfaef0a0b8559884a5
NidhiKapoor18 <kapoornidhi0009@gmail.com>
2026-09-21 09:21:23 +0530

    Create ex5.c

---
+++ CHAPTER 4 : OPERATORS AND EXPRESSIONS/example5.c
@@ -0,0 +1,25 @@
+/*Author: Nidhi Kapoor
+DDU IT SEM 1
+Roll no 076
+Date: 20/09/26*/
+
+#include<stdio.h>
+#include<conio.h>
+int main()
+    {
+    // USE OF RELATIONAL AND BITWISE OPERATORS
+    int a;
+
+    //Here AND operator is used and both the condtions are to be checked.
+    a = 5<=8 && 6!=5;
+    printf("%d", a);
+     return 0;
+    }
+
+
+
+
+
+
+
+}
